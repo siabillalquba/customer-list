de85e5e146c8c56b2c6981b92e5c1a51896cdc4c
Sia Billal Quba <siabillal@gmail.com>
2025-04-09 00:24:35 +0700

    func: add more contacts & edit console log function

--- script.js
+++ script.js
@@ -2,8 +2,32 @@ const contacts = [
   {
     name: "Sia Billal Quba",
     email: "siabillal@gmail.com",
-    phone: "+6287728746499",
+    phone: "+6281122334455",
+    age: 28,
+    id: 1,
+  },
+  {
+    name: "Monkey D Luffy",
+    email: "mugiwara@pirates.com",
+    phone: "+6282233445566",
+    age: 25,
+    id: 2,
+  },
+  {
+    name: "Roronoa Zoro",
+    email: "marimo@pirates.com",
+    phone: "+6283344556677",
+    age: 26,
+    id: 3,
   },
 ];
 
-console.log(contacts);
+for (let i = 0; i < contacts.length; i++) {
+  console.log(`
+    ID          : ${contacts[i].id}
+    Full Name   : ${contacts[i].name}
+    Age         : ${contacts[i].age}
+    Phone       : ${contacts[i].phone}
+    Email       : ${contacts[i].email}
+  `);
+}
