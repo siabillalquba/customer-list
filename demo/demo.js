ba3d7dd82cf5e31902f08dea90fb6744d02939b9
Sia Billal Quba <siabillal@gmail.com>
2025-04-12 22:14:01 +0700

    funct: create add function & demo

--- demo/demo.js
+++ demo/demo.js
@@ -12,48 +12,21 @@ const contacts = [
     },
     isFavorited: true,
   },
-  {
-    id: 2,
-    age: 25,
-    name: "Monkey D Luffy",
-    email: "mugiwara@pirates.com",
-    phone: "+2182233445566",
-    address: {
-      street: "Foosha Village 1",
-      city: "East Blue",
-      country: "Indonesia",
-    },
-    isFavorited: true,
-  },
-  {
-    id: 3,
-    age: 26,
-    name: "Roronoa Zoro",
-    email: "marimo@pirates.com",
-    phone: "+2183344556677",
-    address: {
-      street: "Shimotsuki Village 1",
-      city: "East Blue",
-      country: "Indonesia",
-    },
-    isFavorited: false,
-  },
 ];
 
 function displayContacts() {
   for (let i = 0; i < contacts.length; i++) {
     const contact = contacts[i];
     console.log(`
-    ID: ${contact.id}  
-    Full Name   : ${contact.name}
-    Age         : ${contact.age} years old
-    Phone       : ${contact.phone}
-    Email       : ${contact.email}
-    Address     : ${contact.address.street}, ${contact.address.city}, ${
+      Full Name   : ${contact.name}
+      Age         : ${contact.age} years old
+      Phone       : ${contact.phone}
+      Email       : ${contact.email}
+      Address     : ${contact.address.street}, ${contact.address.city}, ${
       contact.address.country
     }
-    ${contact.isFavorited ? "Favorited" : ""}
-  `);
+      ${contact.isFavorited ? "Favorited" : ""}
+    `);
   }
 }
 
